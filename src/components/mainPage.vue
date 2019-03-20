ed5624e3ccf1e6b881e85e220a6cef7ae32a9abf
wumengya1990 <wumengya@aliyun.com>
2019-03-20 11:45:27 +0800

    添加新内容

---
+++ src/components/mainPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="mainPage">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'mainPage',
+    data() {
+        return {
+            
+        }
+    },
+    mounted() {
+        
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/top.vue
+++ src/components/top.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="top">
-
+        <div class="logo">{{login}}</div>
+        <nav>
+            <router-link>栏目一</router-link>
+            <router-link>栏目二</router-link>
+            <router-link>栏目三</router-link>
+        </nav>
     </div>
 </template>
 
@@ -9,7 +14,7 @@ export default {
     name:'top',
     data(){
         return{
-
+            login:'后台演示'
         }
     },
     mounted(){
@@ -21,6 +26,6 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
